f31544e1b49a8fff46e57efcca90ffa192de4059
Todd Hardeman <toddhardeman6@gmail.com>
2025-04-08 12:53:14 -0700

    updates

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const addButton = document.getElementById('addButton')
 addButton.addEventListener('click', function(event) {
     event.preventDefault(); 
     
-    if (newTask.value.trim() !== "") { 
+    if (newTask.value !== "") { 
         let task = document.createElement('li'); 
         task.appendChild(document.createTextNode(newTask.value)); 
+
         TaskList.appendChild(task); 
         newTask.value = ''; 
     }})
+
