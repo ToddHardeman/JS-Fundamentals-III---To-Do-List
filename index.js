db55b2397d8e6b6efd715f3f0a0beae8cafc1537
Todd Hardeman <toddhardeman6@gmail.com>
2025-04-08 15:05:12 -0700

    final push for the TODO web app

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const addButton = document.getElementById('addButton')
 
 addButton.addEventListener('click', function(event) {
     event.preventDefault(); 
-    
+
+// adding a task
     if (newTask.value !== "") { 
         let task = document.createElement('ul');
         let taskCheckbox = document.createElement('input'); 
         taskCheckbox.type = 'checkbox';
+
         
-        task.append(taskCheckbox, document.createTextNode(newTask.value));
+        let taskItem = document.createElement('div');
+        let taskText = document.createTextNode(newTask.value);
+        taskItem.append(taskCheckbox, taskText);
+        taskItem.classList.add('task-item');
+        task.appendChild(taskItem);
 
         TaskList.appendChild(task); 
 
         newTask.value = ''; 
 }})
+
